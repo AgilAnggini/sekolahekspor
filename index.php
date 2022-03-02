2db9fab0bb6e23c05881a292b4a456e22641ad92
AgilAnggini <73634961+AgilAnggini@users.noreply.github.com>
2022-03-02 22:15:57 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width-device-width, initial-scale-1.0">
+
+<title>Belajar Sekolah Ekspor</title>
+</head>
+
+	<body>
+		<h1>Agil Anggini - 220013 </h1>
+
+		<?php
+			//operator --> aritmatika
+			$a=25;
+			$b=5;
+			$c=true;
+			$d=false;
+			$e=true;
+			$g=false;
+			 
+
+			//cara aritmatika
+			echo "aritmatika";
+
+			echo $a+$b;
+			echo "<br />"; 
+			echo $a-$b;
+			echo "<br />";
+			echo $a*$b;
+			echo "<br />";
+			echo $a/$b;
+			echo "<br />";
+			echo $a%$b;
+			echo "<br />";
+			echo "<br />";
+
+			//increment & decrement
+			echo "increment & decrement";
+			echo "<br />";
+			echo $a;
+			$a--;
+			echo "<br />";
+			echo $b;
+			$a++;
+			echo "<br />";
+
+			echo "relasi";
+			echo "<br />";
+			echo $a>$b;
+			echo "<br />";
+			echo $a<$b;
+			echo "<br />";
+			echo $a==$a;
+			echo "<br />";
+			echo $a!=$b;
+			echo "<br />";
+			echo $a<=$b;
+			echo "<br />";
+			echo $a>=$b;
+			echo "<br />";
+
+			echo "logika";
+			echo "<br />";
+			echo $f = $c&&$d;
+			echo "<br />";
+			echo $f = $c&&$e;
+			echo "<br />";
+			echo $f = $c&&$g;
+			echo "<br />";
+			echo $f = $c&&$c;
+			echo "<br />";
+			echo $f = $c||$d;
+			echo "<br />";
+
+			echo "ternary";
+			echo "<br />";
+			echo $a==$b ?true:false;
+			echo "<br />";
+			echo $a<$b ?true:false;
+			echo "<br />";
+			echo $a>$b ?true:false;
+			echo "<br />";
+			?>
+	</body>
+
+</html>
