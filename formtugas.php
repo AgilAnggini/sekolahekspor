8caa6f368c0f69ed1f89140229bc1cb1a578b099
AgilAnggini <73634961+AgilAnggini@users.noreply.github.com>
2022-03-13 23:18:10 +0800

    Create formtugas.php

---
+++ formtugas.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width-device-width, initial-scale-1.0">
+
+<title>Belajar Sekolah Ekspor</title>
+</head>
+
+	<body>
+		<h1>Agil Anggini - 220013 </h1>
+
+    <form>
+        <div>
+            <label>TUJUAN PEMBERANGKATAN</label> <br>
+            <input name="tujuan" type="text" placeholder="pilih tempat tujuan">
+
+			  <select id="tujuan" name="tujuan">
+			    <option value="volvo">Volvo</option>
+			    <option value="saab">Saab</option>
+			    <option value="fiat">Fiat</option>
+			    <option value="audi">Audi</option>
+			  </select>
+        </div>
+         <div>
+            <label>JUMLAH TIKET YANG INGIN DIBELI</label> <br>
+            <input name="jumlah" type="text" placeholder="Masukkan jumlah tiket yang diinginkan">
+        </div>
+        <div>
+            <button>Submit</button>
+        </div>
+    </form>
+
+
+		<?php
+
+			$username = @$_GET['username'];
+			$username = @$_GET['username'];
+			$username = @$_GET['username'];
+			$username = @$_GET['username'];
+			$username = @$_GET['username'];
+			$username = @$_GET['username'];
+			$alamat = @$_GET['alamat'];
+				if ($username) {
+				    echo "<strong>username:</strong> {$username} <br>";
+				}
+				if ($username) {
+				    echo "<strong>username:</strong> {$username} <br>";
+				}
+				if ($username) {
+				    echo "<strong>username:</strong> {$username} <br>";
+				}
+				if ($username) {
+				    echo "<strong>username:</strong> {$username} <br>";
+				}
+				if ($username) {
+				    echo "<strong>username:</strong> {$username} <br>";
+				}
+				if ($username) {
+				    echo "<strong>username:</strong> {$username} <br>";
+				}
+
+				if ($alamat) {
+				    echo "<strong>Alamat:</strong> {$alamat} <br>";
+			}
+
+		?>
+
+
+	</body>
+
+</html>
